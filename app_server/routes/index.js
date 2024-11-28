3add864860f42955ab95b25f355fdbe735c9233c
ric-ant <ric.antonello@gmail.com>
2024-11-28 01:07:43 -0300

    primeiro commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var ctrlMain = require('../controllers/main');
+
+
+
+var homepageController = function(req,res){
+  res.render('index', {title:'Express'});
+};
+
+
+/* GET home page. */
+router.get('/', homepageController); 
+  
+
+module.exports = router;
